0b4263edd0a84579aa4f8cf227087d54c47f63e6
MI SHAJIB <mishajib222@gmail.com>
2022-03-25 00:40:45 +0600

    admin register decorator used

--- blog/admin.py
+++ blog/admin.py
@@ -3,14 +3,12 @@ from blog.models import Category, Post
 
 
 # Register your models here.
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    exclude = ('slug',)
-    list_display = ('title', 'slug',)
+    exclude = ['slug']
+    list_display = ['title', 'slug']
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'user',)
-
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Post, PostAdmin)
+    list_display = ['title', 'content', 'user']
